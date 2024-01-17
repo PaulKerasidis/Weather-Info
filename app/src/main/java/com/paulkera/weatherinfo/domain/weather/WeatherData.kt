e6e8c38e41b8dcc6e87fc311881b1732bbdbfa6a
Pkerasidis <p.kerasidis@icloud.com>
2024-01-17 12:40:25 +0200

    Api and Mappers

---
+++ app/src/main/java/com/paulkera/weatherinfo/domain/weather/WeatherData.kt
@@ -0,0 +1,12 @@
+package com.paulkera.weatherinfo.domain.weather
+
+import java.time.LocalDateTime
+
+data class WeatherData(
+    val time: LocalDateTime,
+    val tempetaruteCelsius: Double,
+    val pressure: Double,
+    val windSpeed: Double,
+    val humidity: Double,
+    val weatherType: WeatherType
+)
---
+++ app/src/main/java/com/paulkera/weatherinfo/domain/weather/WeatherInfo.kt
@@ -0,0 +1,6 @@
+package com.paulkera.weatherinfo.domain.weather
+
+data class WeatherInfo(
+    val weatherDataPerDay: Map<Int, List<WeatherData>>,
+    val currentWeatherData: WeatherData?
+)
